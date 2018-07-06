59e808a2d3b8605bfd572a7f2ed66880f8e11bb4
Alexis Williams <will1397@purdue.edu>
2018-07-05 21:34:34 -0400

    Added base upload picture functionality

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path')
 var fs = require('fs');
 
 var mongo = require('mongodb');
+var binary = require('mongodb').Binary;
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 var bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
@@ -34,7 +34,7 @@ app.post('/login', function(req, res, next) {
     form.keepExtensions = true
 
 	form.parse(req, function(err, fields, files) {
-        //Query database for username and hashed psasword
+        //Query database for username and hashed password
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
             var dbo = db.db("mydb");
@@ -60,11 +60,23 @@ app.post('/upload', function(req, res, next) {
     form.multiples = true;
     form.keepExtensions = true
 
-    //form.uploadDir = (__dirname + '/uploads');
 	form.parse(req, function(err, fields, files) {
-        console.log(fields);
-        console.log(files);
-        res.render('Admin', {msg: 'Success'});
+        console.log(fields.title_text);
+        console.log(files.picture_file.name);
+
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            var bdata = fs.readFileSync(files.picture_file.path);;
+            var obj = {title: fields.title_text, description: fields.des_text, picture: files.picture_file.name, data: binary(bdata)};
+
+            dbo.collection("pictures").insertOne(obj, function(err, re) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+                res.render('Admin', {msg: 'Success'});
+            });
+        });
     });
 });
 
