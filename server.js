2a6dd491fa34b7f50bddfdd3281dffdfce7b7243
Alexis Williams <will1397@purdue.edu>
2018-07-07 13:42:24 -0400

    Basic image viewing functionality added

--- server.js
+++ server.js
@@ -16,7 +16,15 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('Main');
+    MongoClient.connect(url, function(err, db) {
+        var dbo = db.db("mydb");
+        dbo.collection("pictures").find({"picture": {$exists : true}}).toArray(function(err, result) {
+            result.forEach(function(value) {
+                value.data.buffer = new Buffer(value.data.buffer).toString('base64');
+            });
+            res.render('Main', {cards : result}) //can return empty or non-empty result
+        });
+    });
 });
 
 app.get('/Login', function(req, res) {
@@ -28,7 +36,7 @@ app.get('/Admin', function(req, res) {
     res.render('Login', {msg: ''});
 });
 
-app.post('/login', function(req, res, next) {
+app.post('/login', function(req, res) {
     var form = new formidable.IncomingForm();
     form.multiples = true;
     form.keepExtensions = true
@@ -39,9 +47,11 @@ app.post('/login', function(req, res, next) {
             if (err) throw err;
             var dbo = db.db("mydb");
 
-            dbo.collection("pictures").find({"user": fields.user}).toArray(function(err, result) {
+            dbo.collection("pictures").find({"user" : fields.user}).toArray(function(err, result) {
                 if (err) throw err;
                 bcrypt.compare(fields.pass, result[0].pass, function(err, re) {
+                    if (err) throw err;
+
                     if (re === true) {
                         res.render('Admin', {msg: ''});
                     }
@@ -55,20 +65,17 @@ app.post('/login', function(req, res, next) {
     });
 });
 
-app.post('/upload', function(req, res, next) {
+app.post('/upload', function(req, res) {
     var form = new formidable.IncomingForm();
     form.multiples = true;
     form.keepExtensions = true
 
 	form.parse(req, function(err, fields, files) {
-        console.log(fields.title_text);
-        console.log(files.picture_file.name);
-
         MongoClient.connect(url, function(err, db) {
             if (err) throw err;
             var dbo = db.db("mydb");
             var bdata = fs.readFileSync(files.picture_file.path);;
-            var obj = {title: fields.title_text, description: fields.des_text, picture: files.picture_file.name, data: binary(bdata)};
+            var obj = {title : fields.title_text, description : fields.des_text, picture : files.picture_file.name, data : bdata};
 
             dbo.collection("pictures").insertOne(obj, function(err, re) {
                 if (err) throw err;
